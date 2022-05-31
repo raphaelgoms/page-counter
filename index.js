91b686dd136a3535eab2f99dffcc3336bf1f2e6e
Raphael Gomes Santos <raphael.gomes@ibm.com>
2022-05-31 17:16:56 -0300

    v1.0

--- index.js
+++ index.js
@@ -3,12 +3,39 @@
 const http = require('http');
 const url = require('url');
 
+const httpResponse = require('./lib/httpresponse');
+const pagehit = new (require('./lib/pagehit'))();
+
 const port = 3000;
 
 http.createServer((req, res) => {
 
+    let count = pagehit.count(req);
+
+    if (!count) {
+        httpResponse({ res, status: 400, content: "No referrer" });
+        return;
+    }
+
     let uri = new URL(req.url, "http://localhost:3000/").pathname;
-    res.end(uri);
+    switch (uri) {
+
+        // JavaScript document.write() counter
+        case '/counter.js':
+
+            httpResponse({
+                res,
+                mime: 'application/javascript',
+                content: `document.write('<span class="pagecounter">${count}</span>');`
+            });
+            break;
+
+        // error: invalid HTTP request
+        default:
+            httpResponse({ res, status: 404, content: 'Not found' });
+            break;
+
+    }
 
 }).listen(port);
 
